36c1635fd2c34df0e9d29c02083ea9cccfb8cba6
glacierbliss <andybliss@gmail.com>
2024-12-03 00:26:51 -0900

    Got plot to work, code is messy.

--- create_tide_file.py
+++ create_tide_file.py
@@ -1,10 +1,10 @@
-#AKB started to convert... need internet. need to add pandas to conda env
 import requests
 import pickle
 import pandas as pd
 from pathlib import Path
 
-''' Downloads tide data from NOAA at Tarr Inlet, you can change station number to retrieve data from other places'''
+''' Downloads tide data from NOAA at Tarr Inlet, you can change station number 
+to retrieve data from other places. This code uses GMT for time zone. '''
 
 # specify the pickle file path
 pickle_file_path = Path('G:/Glacier/GD_ICEH_iceHabitat/data') #Path would take care of trailing slash
@@ -57,15 +57,58 @@ df = df.rename(columns={'t': 'date', 'v': 'depth_tide_ellipsoid'})
 df['date'] = pd.to_datetime(df['date'])
 
 # save the DataFrame to the pickle file
-df.to_pickle(pickle_file_path/output_filename)
+df.to_pickle(pickle_file_path/output_filename) #pathlib overloads division '/'
 
+########
 #AKB added
-# plot the tide levels
-df.set_index('date', inplace=True) # Set 'date' as the index
-df.plot(df.index,df['depth_tide_ellipsoid'])
+#########
+dfp = pd.read_pickle(pickle_file_path/output_filename)
+df=dfp
+# was having trouble plotting due to type:
+type(df['depth_tide_ellipsoid'])
+# <class 'pandas.core.series.Series'>
+type(df['depth_tide_ellipsoid'][1])
+# <class 'str'>
+df.dtypes
+df['depth_tide_ellipsoid'].dtypes
 
-df.plot(x=df['date'],y=df['depth_tide_ellipsoid'])
+#astype(str)
+#to_numeric
+df['depth_tide_ellipsoidNum']=pd.to_numeric(df['depth_tide_ellipsoid'])
+# A value is trying to be set on a copy of a slice from a DataFrame.
+# Try using .loc[row_indexer,col_indexer] = value instead
+df[:,'depth_tide_ellipsoidNum']=pd.to_numeric(df['depth_tide_ellipsoid'])
+#raise InvalidIndexError(key)
+df.loc[:,'depth_tide_ellipsoidNum']=pd.to_numeric(df['depth_tide_ellipsoid'])
+#worked
+df.loc[:,'depth_tide_ellipsoidAlt']=df['depth_tide_ellipsoid']
+# df['depth_tide_ellipsoidAlt']=pd.to_numeric(df['depth_tide_ellipsoid'])
+df.loc[:,'depth_tide_ellipsoid']=pd.to_numeric(df['depth_tide_ellipsoid'])
+
+import matplotlib.pyplot as plt
+# # plot the tide levels
+# df.set_index('date', inplace=True) # Set 'date' as the index
+# df.plot(df.index,df['depth_tide_ellipsoid'])
+# 
+# df.plot(x=df['date'],y=df['depth_tide_ellipsoid'])
+# plt.show()
+# 
+# df.plot()
+# plt.show()
+df2=df
+df=df2
+df=df[0:50]
+plt.figure(figsize=(10, 6))
+# plt.plot(df.index, df['depth_tide_ellipsoid'])
+# plt.scatter(df.index, df['depth_tide_ellipsoid'])
+plt.plot(df['date'], df['depth_tide_ellipsoid'])
+# plt.scatter(df['date'], df['depth_tide_ellipsoid'])
+plt.xlabel('Date')
+plt.xticks(rotation=45, ha='right')
+plt.ylabel('Value')
+plt.title('Time Series Plot')
+plt.grid(False)
+plt.tight_layout() #helps, but not enough
+plt.subplots_adjust(bottom=0.2,left=.15)
 plt.show()
 
-df.plot()
-plt.show()
