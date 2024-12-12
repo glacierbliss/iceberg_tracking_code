d9dc10af958c9ea1dcf1728bf477ab25265f77ef
glacierbliss <andybliss@gmail.com>
2024-12-11 17:03:46 -0900

    Minor changes (hoping order of DATA doesn't matter)

--- create_calibration_file.py
+++ create_calibration_file.py
@@ -9,24 +9,24 @@ data = {
     'camera': ['cam1'],
     'image': ['20190731-201250.jpg'],
     # 'imagefolder': ['/hdd3/opensource/iceberg_tracking/data/sample_data/cam1'],
-    'imagefolder': Path('G:/Glacier/GD_ICEH_iceHabitat/data'),
+    'imagefolder': Path('G:/Glacier/GD_ICEH_iceHabitat/data/cam1'),
+    'mask': ['mask_20190731-201250.shp'],
     'start_day': [20190724],
     'end_day': [20190726],
     'start_time': ['13:00'],
     'tracking_duration': [16],
     'tracking_interval': [60],
-    'sensor_width': [22.3],
     'easting': [377280.39],
     'northing': [6525846.97],
     'elevation': [261.3],
     'antenna_height': [0],
+    'sensor_width': [22.3],
     'image_width': [3456],
     'image_height': [2304],
     'crop_left': [0],
     'crop_right': [0],
     'crop_top': [0],
     'crop_bottom': [0],
-    'mask': ['mask_20190731-201250.shp']
 }
 df1 = pd.DataFrame(data)
 
@@ -51,9 +51,15 @@ df ['psi_max'] = df['psi_min'] + 4
 #combine dataframes
 df1_repeated = pd.concat([df1]*len(df), ignore_index=True)
 df_combined = pd.concat([df1_repeated, df], axis=1)
+df_combined.dtypes
+
+#TODO: Side quest - create calibration_input_2019 from code:
+#create df_single
+df_single=df_combined.head(1)
+df_single.to_excel(Path('G:/Glacier/GD_ICEH_iceHabitat/data/calibration_input_2019_TEST.xlsx'), index=False)
 
 #Save to excel
-# df_combined.to_excel('data/sample_data/calibration_combinations_all.xlsx', index=False)
-df_combined.to_excel(Path('G:/Glacier/GD_ICEH_iceHabitat/data/sample_data/calibration_combinations_all.xlsx'), index=False)
+# df_combined.to_excel('data/calibration_combinations_all.xlsx', index=False)
+df_combined.to_excel(Path('G:/Glacier/GD_ICEH_iceHabitat/data/calibration_combinations_all.xlsx'), index=False)
 # df_combined.to_excel(Path(r'D:\U\Glacier\GD_ICEH_iceHabitat/JUNK_calibration_combinations_all.xlsx'), index=False)
 
